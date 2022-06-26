94ed8f75436bff0b42f3a6105bc5df3ff7332d02
anil babu <anilb9850@gmail.com>
2022-06-26 23:52:27 +0530

    insert data into table

--- db.js
+++ db.js
@@ -1,18 +1,21 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'nodeapp'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodeapp",
 });
 
-  
-const connectoMysql =()=>{
-   connection.connect(function(err) {
+const connectoMysql = () => {
+  connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Myqsl successfully connected ");
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company abc', 'Highway 88')";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
   });
+};
 
-}
-
-module.exports=connectoMysql
+module.exports = connectoMysql;
